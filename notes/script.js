b89281f3171536f35e37035231f50aec612ddc9f
srijan <sribasnet07@gmail.com>
2025-08-04 22:10:25 +0545

    minor bug fix

--- notes/script.js
+++ notes/script.js
@@ -4,8 +4,7 @@ let isSearchMode = false;
 let currentFolder = null;
 
 async function fetchData(url) {
-  //const baseUrl = "http://34.171.46.200:8000";
-  const baseUrl = "http://localhost:8000";
+  const baseUrl = "http://34.171.46.200:8000";
   try {
     const response = await fetch(baseUrl + url);
     if (!response.ok) {
